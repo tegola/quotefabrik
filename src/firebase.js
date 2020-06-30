9ea54c26f8b19f02fbdc38a36b10c807a2e114e9
Alan Sprecacenere <alan@qreate.it>
2020-07-01 01:49:57 +0200

    Cleanup indentation and unused files

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 export default firebase .initializeApp({
-	apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-	authDomain: 'quotefabrik.firebaseapp.com',
-	databaseURL: 'https://quotefabrik.firebaseio.com',
-	projectId: 'quotefabrik',
-	storageBucket: 'quotefabrik.appspot.com',
-	messagingSenderId: '858466584708',
-	appId: '1:858466584708:web:13b73a5f8045cf2eb7dad1'
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: 'quotefabrik.firebaseapp.com',
+  databaseURL: 'https://quotefabrik.firebaseio.com',
+  projectId: 'quotefabrik',
+  storageBucket: 'quotefabrik.appspot.com',
+  messagingSenderId: '858466584708',
+  appId: '1:858466584708:web:13b73a5f8045cf2eb7dad1'
 })
 
 const db = firebase.firestore()
@@ -17,7 +17,7 @@ const auth = firebase.auth()
 const user = auth.currentUser
 
 export {
-	db,
-	auth,
-	user
+  db,
+  auth,
+  user
 }
